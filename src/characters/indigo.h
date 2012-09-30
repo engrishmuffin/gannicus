abe5cf7d39aaed5009580e2f1d3b2ebf6f520b51
H Forrest Alexander <h.forrest.alexander@gmail.com>
2012-09-29 18:38:10 -0700

    there

--- src/characters/indigo.h
+++ src/characters/indigo.h
@@ -1,7 +1,7 @@
 #ifndef INDIGO_INCLUDED
 #define INDIGO_INCLUDED
-#include "character.h" //update on generic file in tarball
-class indigo : public character {
+#include "../engine/character.h" 
+	class indigo : public character {
 public:
 	indigo();
 };
