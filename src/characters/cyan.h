6ea6c4fecfe2e8d6ba26e54eeb3e861979b651c8
Alex Kelly <alex.page.kelly@gmail.com>
2012-10-01 00:53:28 -0700

    Setting up more stuff

--- src/characters/cyan.h
+++ src/characters/cyan.h
@@ -4,11 +4,12 @@
 class cyan : public character {
 public:
 	cyan();
+	instance * ball;
 };
 
 class blink : public action {
 public:
 //	virtual bool cancel(action*, int&, int&); //Cancel allowed activate. Essentially: is action Lvalue allowed given the current state of action Rvalue?
-
+	cMove * cover;
 };
 #endif
