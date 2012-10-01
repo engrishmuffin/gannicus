4662ceef16c45a92c3b1bc4dac44b3939e9d12ac
Alex Kelly <alex.page.kelly@gmail.com>
2012-09-30 19:10:46 -0700

    Starting to set up thingas majig

--- src/characters/cyan.h
+++ src/characters/cyan.h
@@ -5,4 +5,10 @@ class cyan : public character {
 public:
 	cyan();
 };
+
+class blink : public action {
+public:
+//	virtual bool cancel(action*, int&, int&); //Cancel allowed activate. Essentially: is action Lvalue allowed given the current state of action Rvalue?
+
+};
 #endif
