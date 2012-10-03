579e270a5cbfeaec3dfc094e17ecf1aa3eb506fe
H Forrest Alexander <h.forrest.alexander@gmail.com>
2012-10-02 19:23:47 -0700

    added a stub

---
+++ src/opera/opera.h
@@ -0,0 +1,12 @@
+/** Everything is currently a stub!! 
+ *
+ * This is an object that can be sent messages once per frame **/
+
+#include<SDL/SDL_mixer.h> //until we find something better. really could be anything
+#include "statistics.h" //definition of some data structures should probably be there
+#include "synapse.h" //definition of some other data structures specialized for message passing, possibly all functions. needs to be really clean and can be extremely simple for now.
+
+
+
+
+
