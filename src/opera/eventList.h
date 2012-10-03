233414b798740028d49dd110ce743df47ed13b8f
H Forrest Alexander <h.forrest.alexander@gmail.com>
2012-10-03 14:38:06 -0700

    these are the four basic event types initially defined. they should each have a constructor that enables supplying them with appropriate .ogg files for a character or stage. inherited or parametrized versions of these can be later used if e.g. particular characters have non-standard loop lengths, etc. but for now, the goal is to get something simple working in not too long

---
+++ src/opera/eventList.h
@@ -0,0 +1,5 @@
+#include "events/ambience.h" //eg background noise, character rhythm
+#include "events/effect.h" //eg for moves
+#include "events/leitmotif.h" //eg for winning/queenguitars`
+#include "events/ostinato.h" //eg for losing/tie
+
