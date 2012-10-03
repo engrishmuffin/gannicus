cab0a8db947d035686388c4b4f79c3b4386be80f
H Forrest Alexander <h.forrest.alexander@gmail.com>
2012-10-03 14:00:22 -0700

    fleshing out stubs slowly

--- src/opera/opera.cc
+++ src/opera/opera.cc
@@ -1,23 +1,39 @@
-opera()
-{
-	//tare the current frame so that "number of frames elapsed" can be kept track of
-	loadOgg();
+#include "opera.h"
+#include <SDL/SDL_mixer.h>
+
+
+opera::opera()
+{	
+	//tare the current frame so that "number of frames elapsed" can be tracked
+	//load each event
+	currentVolume = defaultVolume;
 }
-~opera(){}
 
-void loadOgg(){}
-
-void tick()
+opera::~opera()
 {
-	
+}
+
+
+void opera::beat()
+{	
 	listen(p1Score, p2Score);
 	//for i in events
-	//	event.tick();
+	//	event.beat();
 }
 
-void listen(operaScore, operaScore)
+void opera::listen(operaScore, operaScore)
 {
 	//Take in an operaSignal.
 	//Check if even
 	//Play the appropriate operaEvent
 }
+
+float opera::volume(){return currentVolume;}
+//float->currentVolume with bounds
+float opera::volume(newVolume){
+	if newVolume < maxVolume {
+		if newVolume > minVolume {
+			currentVolume = newVolume;
+		} else currentVolume = minVolume;
+	} else newVolume = maxVolume;
+
--- src/opera/opera.h
+++ src/opera/opera.h
@@ -1,3 +1,6 @@
+#ifndef OPERA
+#define OPERA
+
 /** Everything is currently a stub!! 
  *
  * This is an object that can be sent messages once per frame 
@@ -5,16 +8,22 @@
  * It won't at any point attempt to define the in-game state that should result in those messages.
  * **/
 
-#include<SDL/SDL_mixer.h> //until we find something better. really could be anything
 
 class opera
 {
 public:
-	 opera(); 
-	~opera();
-	void loadOgg(); 
-	void tick(); /** things to do every frame **/
+	 opera(); //load all of the operaEvents
+	~opera(); //unload everything
+	void beat(int); /** things to do every frame. accepts an argument as to what frame it is **/
 	void listen(operaScore, operaScore); //take in a signal and set appropriate things
+	void mix(); /**allocate volume to the events based on what they request **/
+	float volume(); //get current master volume
+	float volume(int) //set current master volume
+private:
+	float currentVolume;
+	const float maxVolume = 100.0;
+	const float minVolume = 0.0;
+	const float defaultVolume = 50.0;
 }
 
 class operaEvent
@@ -22,23 +31,31 @@ class operaEvent
 public:
 	 operaEvent(); //get the correct .ogg file
 	~operaEvent(); //make sure silent
-	fire(); //start, or amplify/sustain
-	play(); //start playing IF the frame is the correct number, AND recently fired
-	decay(); //decay by half or just stop if under a threshold AND last_fired > threshold
-	tick(); //things that happen every frame: fire
+	void beat(); //things that happen every frame: fire, play, decay
+	void activate(); //start, or amplify/sustain
+	void play(); //start playing IF the frame is the correct number, AND recently fired
+	void decay(); //decay by half or just stop if under a threshold AND last_fired > threshold
 private:
-	float volume; //as a portion of the total
-	int frames; //number of frames the sound occupies and how often it can fire 
+	float currentVolume; //as a portion of the total
+	float thresholdVolume; //to be considered active
+	int frames; //number of frames the sound occupies
+	int rateModulus; //can only play on currentFrame
 	int lastFired; //checked by play and decay, incremented by decay, zeroed by fire
 	const int decayThreshold;
 }
 
 //Make descendent classes of operaEvent for the event types, since they have default volume/frames values, different conditions in fire() before zeroing, etc. 
-//for each player: streak, winning, even, losing, exists.
+//for each player: streak, winning, even, losing, move player
 //for stage: ambience, interlude
+
+//for NOW:
+//ambience
+//exists
+//winning
+
 struct scoreMask
 {
-	/*Obviously, more information is good, but  */
+	/*Obviously, more information is good, but for now... */
 	bool winning:1;
 	bool streak:1; //queen guitars
 	bool struggling:1; //e.g. if both have this, they're even
@@ -49,4 +66,4 @@ typedef union {
 	scoreMask s;
 } operaScore;
 
-
+#endif 
