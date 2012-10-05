2f46dcdc74b24d0699635ed818ac2e1a7db786d3
H Forrest Alexander <h.forrest.alexander@gmail.com>
2012-10-04 22:08:47 -0700

    pretty simple right now, but hey it works

--- src/opera/test.cc
+++ src/opera/test.cc
@@ -1,7 +1,14 @@
+//build with g++ operaEvent.cc test.cc -lconfig -lSDL -lSDL_mixer
+
+
 #include "operaEvent.h"
 #include <iostream>
+#include <SDL/SDL_mixer.h>
 
 int main(int argc, char* argv[])
 {
+	Mix_OpenAudio(44100, AUDIO_S16SYS, 2, 2048);
 	operaEvent event(argv[1], 1);
+	event.play();
+	std::cin.get();
 }
