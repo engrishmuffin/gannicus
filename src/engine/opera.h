c63534f25b9f40630b78787e0f5c4f8987305fec
H Forrest Alexander <h.forrest.alexander@gmail.com>
2013-06-24 19:38:29 -0700

    ideas

--- src/engine/opera.h
+++ src/engine/opera.h
@@ -5,12 +5,16 @@
 using std::string;
 #ifndef OPERA_INCLUDED
 #define OPERA_INCLUDED
+
+enum Degree {Tonic, Supertonic, Mediant, Subdominant, Dominant, Submediant, LeadingTone};
+
+
 struct note {
 	int octave;
 	int accidental;
-	int degree;
-	int velocity;
-	int duration;
+    Degree degree;
+	unsigned int velocity;
+	size_t duration;
 //	int modulation;
 };
 
@@ -26,4 +30,15 @@ public:
 	virtual void init();
 	virtual ~soundScape();
 };
+
+class voice {
+    sample attack, sustain, decay; //I'll try to make attack n / 60 seconds, sustain 1/60 second, and decay arbitrary length in general, so they map nicely to frames.
+public: 
+    voice(sample, sample, sample); //attack, sustain, decay
+    void play(int, int); //Attack, then begin sustain and keep sustaining.
+                         //Arguments are volume and duration
+
+    void stop(); //Stop even if still decaying.
+    ~voice();
+    
 #endif
