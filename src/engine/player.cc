c355799b33fc993d5fc11a98cd66e75206425db2
Alex Kelly <eshink@gmail.com>
2013-08-14 02:14:01 -0700

    I think this bug be fix

--- src/engine/player.cc
+++ src/engine/player.cc
@@ -326,6 +326,8 @@ void controller::writeConfig(int ID)
 
 bool player::reversalPossible()
 {
+	if(!current.move) return false;
+	if(current.connect > current.move->hits) return false;
 	if(current.move->state[current.connect].i & 1) return false;
 	if(current.move->linkable) return true;
 	if(current.counter < 0 && current.counter > -11) return true;
